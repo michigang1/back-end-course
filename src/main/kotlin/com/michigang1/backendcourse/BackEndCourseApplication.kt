ebc4ba419685623f5ebbdb4a377165bbc98fa0df
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-11-12 19:22:19 +0100

    Initialized Spring Boot project using Kotlin language and Gradle-Koltin build system

---
+++ src/main/kotlin/com/michigang1/backendcourse/BackEndCourseApplication.kt
@@ -0,0 +1,11 @@
+package com.michigang1.backendcourse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackEndCourseApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackEndCourseApplication>(*args)
+}
